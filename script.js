4e1217b46feeef69f8ae8cad6fc23cbe780ee19a
Skyll_Box <113237888+Damien-Codes@users.noreply.github.com>
2023-02-14 17:58:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // alert("yé souis lo potit chat")
+// Cette fonction sert a ajouter une ligne dans le tableau
 function ajouterLigne(){
     var newLi= document.getElementById("modeleLigne").cloneNode(true);
     var lastLi= document.getElementById("lastLigne");
@@ -7,10 +8,11 @@ function ajouterLigne(){
     pere.insertBefore(newLi,lastLi);
 
 }
+// Cette fonction sert a supprimer une ligne dans le tableau
 function supprimerLigne(){
 	document.getElementById("table_inv").deleteRow(-1);
 }
-
+// Cette fonction sert a calculer les ligne Quantité et prix dans le tableau
 function calculate(){
     var total = 0
 
@@ -21,6 +23,43 @@ function calculate(){
     for (var i = 0; i< listePrix.length;i++) {
         listeTtl[i].value = parseInt(listeQte[i].value)*parseInt(listePrix[i].value);
         total = total + parseInt(listeTtl[i].value);
-        document.getElementById("total").innerHTML = total;
+        document.getElementsByClassName("total").innerHTML = total;
+    }
+// Calcul le Sous-TOTAL
+    var TOTAL = document.getElementsByClassName("TOTAL");
+    TOTAL[0].value = total;
+// Calcul la REMISE
+    var REMISE = document.getElementsByClassName("REMISE");
+// Calcul le Sous-TOTAL et le Sous-TOTAL
+    var TOTALREMISE = document.getElementsByClassName("TOTALREMISE");
+    TOTALREMISE[0].value = TOTAL[0].value - REMISE[0].value;
+
+    var TAUX = document.getElementsByClassName("TAUX");
+
+    // var TAXE = document.getElementsByClassName("TOTALREMISE");
+    // TAXE[0].value = TOTAL[0].value * TAUX[0].value;
+}
+// Cette fonction sert a réinitialiser les ligne du tableau
+function reinitialiser(){
+    total="";
+}
+
+
+function remplir() {
+    let listePrix = document.getElementsByClassName("prix");
+    let listeQte = document.getElementsByClassName("qte");
+    let listeDesc = document.getElementsByClassName("desc");
+
+    const tabDesc = new Array("Portable", "bureau", "ram 8go", "clef usb 16go", "souris", "ecran 17 pouces");
+
+    for (let i=0; i < listeDesc.length; i++) {
+
+        let idesc = Math.floor(tabDesc.length * Math.random());
+        listeDesc[i].value = tabDesc[idesc];
+
+        let qte = Math.floor(10 * Math.random() + 1);
+        let prix = Math.floor(100 * Math.random() + 1);
+        listeQte[i].value = qte;
+        listePrix[i].value = prix;
     }
 }
